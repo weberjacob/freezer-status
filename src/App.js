c6e053fa7bca58edfa4049773f9a27affd2220c2
jacobweber <jacobaweber@gmail.com>
2020-08-19 22:45:02 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Firebase from "firebase";
+import config from "./config";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    Firebase.initializeApp(config);
+
+    this.state = {
+      freezerItems: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state) {
+      this.writeUserData();
+    }
+  }
+
+  writeUserData = () => {
+    Firebase.database()
+      .ref("/")
+      .set(this.state);
+    // console.log("DATA SAVED");
+  };
+
+  getUserData = () => {
+    let ref = Firebase.database().ref("/");
+    ref.on("value", snapshot => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+  };
+
+  clearInputs = () => {
+    this.refs.name.value = "";
+    this.refs.count.value = "";
+    this.refs.uid.value = "";
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let name = this.refs.name.value;
+    let count = this.refs.count.value;
+    let uid = this.refs.uid.value;
+
+    if (uid && name && count) {
+      const { freezerItems } = this.state;
+      const freezerItemIndex = freezerItems.findIndex(data => {
+        return data.uid === uid;
+      });
+      freezerItems[freezerItemIndex].name = name;
+      freezerItems[freezerItemIndex].count = count;
+      this.setState({ freezerItems });
+    } else if (name && count) {
+      const uid = new Date().getTime().toString();
+      const { freezerItems } = this.state;
+      freezerItems.push({ uid, name, count });
+      this.setState({ freezerItems });
+    }
+
+    // this.refs.name.value = "";
+    // this.refs.count.value = "";
+    // this.refs.uid.value = "";
+    this.clearInputs();
+  };
+
+  removeData = item => {
+    const { freezerItems } = this.state;
+    const newState = freezerItems.filter(data => {
+      return data.uid !== item.uid;
+    });
+    this.setState({ freezerItems: newState });
+  };
+
+  updateData = item => {
+    this.refs.uid.value = item.uid;
+    this.refs.name.value = item.name;
+    this.refs.count.value = item.count;
+  };
+
+  incrementCount = item => {
+    const { freezerItems } = this.state;
+    this.refs.uid.value = item.uid;
+    this.refs.name.value = item.name;
+    this.refs.count.value = ++item.count;
+    this.setState({ freezerItems });
+    this.clearInputs();
+  };
+
+  decrementCount = item => {
+    const { freezerItems } = this.state;
+    this.refs.uid.value = item.uid;
+    this.refs.name.value = item.name;
+    this.refs.count.value = --item.count;
+    this.setState({ freezerItems });
+    this.clearInputs();
+  };
+
+  render() {
+    const { freezerItems } = this.state;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              <h1>What's in the freezer?</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xl-12">
+              { freezerItems.map(item => (
+                <div
+                  key={ item.uid }
+                  className="card float-left"
+                  style={{ width: "18rem", marginRight: "1rem" }}
+                >
+                  <div className="card-body">
+                    <h5 className="card-title">{ item.name }</h5>
+                    <p className="card-text"> { item.count }</p>
+                    <button
+                      onClick={() => this.removeData(item)}
+                      className="btn btn-link"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => this.updateData(item)}
+                      className="btn btn-link"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => this.incrementCount(item)}
+                      className="btn btn-link"
+                    >
+                      +1
+                    </button>
+                    <button
+                      onClick={() => this.decrementCount(item)}
+                      className="btn btn-link"
+                    >
+                      -1
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xl-12">
+              <h1>Add new freezer item</h1>
+              <form onSubmit={ this.handleSubmit }>
+                <div className="form-row">
+                  <input type="hidden" ref="uid" />
+                  <div className="form-group col-md-6">
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      ref="name"
+                      className="form-control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label>Count</label>
+                    <input
+                      type="number"
+                      ref="count"
+                      className="form-control"
+                      placeholder="Count"
+                    />
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyBf9NTM90Pwz1kkpjG329BOnmXcwNRw1L8",
+  authDomain: "freezer-status.firebaseapp.com",
+  databaseURL: "https://freezer-status.firebaseio.com",
+  projectId: "freezer-status",
+  storageBucket: "freezer-status.appspot.com",
+  messagingSenderId: "101036633888",
+  appId: "1:101036633888:web:8c250f7492f8795133b145",
+  measurementId: "G-2TZER30XVR"
+};
+
+export default config;
